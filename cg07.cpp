5221aeac487113a80959d541b37c88ab931a7511
tejasmorkar <tejasmorkar@gmail.com>
2019-12-17 15:33:05 +0530

    Cleaning garbage files

--- cg07.cpp
+++ cg07.cpp
@@ -1,14 +1,1 @@
-#include<graphics.h>
 
-int main()
-{
-    int gd = DETECT, gm;
-    initgraph(&gd, &gm, NULL);
-    
-    setcolor(2);
-    circle(100,200,50);
-
-    delay(1000);
-    
-    return 0;
-}
